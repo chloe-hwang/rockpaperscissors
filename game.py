cffc61c3efd68e962b9d046c510f7c042c8d795c
chloe-hwang <ch1248@georgetown.edu>
2022-02-02 13:44:02 -0500

    user choice

--- game.py
+++ game.py
@@ -5,3 +5,10 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+#USER INPUT 
+userchoice = input("Choose one: 'rock', 'paper', 'scissors'!")
+
+print("USER CHOSE:", userchoice)
+
+#VALIDATE USER INPUT 
